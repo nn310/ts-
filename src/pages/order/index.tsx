4a46978bb595ff415714c972e2e57bcab6618d36
zzn <3101216081@qq.com>
2021-10-11 16:38:59 +0800

    订单管理页面基础搭建

---
+++ src/pages/order/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface IOrderProps {}
+
+const Order: React.FunctionComponent<IOrderProps> = (props) => {
+  return <div className="order">order页面</div>;
+};
+
+export default Order;
